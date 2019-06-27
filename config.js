abba0ca652b203351b0d2f2c2d00eba70c40608a
Roshan Padaki <roshanpadaki@gmail.com>
2019-06-26 21:02:52 -0500

    Reworked loading environment variables and the config schema.

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
+require("dotenv").config();
+
 module.exports = {
-  tmioptions: {
-    options: {
-      debug: true
+    tmi: {
+        options: {
+            debug: true
+        },
+        connection: {
+            reconnect: true
+        },
+        identity: {
+            username: process.env.TWITCH_BOT_USERNAME,
+            password: process.env.TWITCH_BOT_OAUTH_TOKEN
+        },
+        channels: ["cdnthe3rd"]
     },
-    connection: {
-      reconnect: true
-    },
-    identity: {
-      username: process.env.TWITCH_USERNAME,
-      password: process.env.TWITCH_OAUTH_TOKEN
-    },
-    channels: ["cdnthe3rd"]
-  }
+    twitchstream: {
+        endpoint: process.env.TWITCH_STREAM_ENDPOINT,
+        streamkey: process.env.TWITCH_STREAM_KEY
+    }
 };
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const tmi = require("tmi.js");
 const config = require("./config");
 
-const client = new tmi.client(config.tmioptions);
+const client = new tmi.client(config.tmi);
 
 function onMessageHandler(target, context, msg, self) {
-  if (self) return;
+    if (self) return;
 }
 
 function onConnectedHandler(addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+    console.log(`* Connected to ${addr}:${port}`);
 }
 
 client.on("message", onMessageHandler);
